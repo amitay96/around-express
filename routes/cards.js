f665c6a7f1f2e78731a6a3407a54fdb0af0fa07e
amitay96 <amitay96@gmail.com>
2022-09-18 21:07:36 +0300

    feature: validator add

--- routes/cards.js
+++ routes/cards.js
@@ -1,4 +1,11 @@
 const router = require("express").Router();
+const {
+  getCards,
+  createCard,
+  deleteCard,
+  likeCard,
+  dislikeCard,
+} = require("../controllers/cards");
 // const fs = require('fs').promises;
 // const path = require('path');
 
