8eae2a7b46784a33a931303d510b8ed862802d2e
amitay96 <amitay96@gmail.com>
2022-09-20 13:41:51 +0300

    feature: server data modification

--- app.js
+++ app.js
@@ -6,12 +6,19 @@ const { PORT = 3000 } = process.env;
 
 const app = express();
 
-mongoose.connect("mongodb://localhost:27017/aroundb");
+app.use((req, res, next) => {
+  req.user = {
+    _id: '632993d9eb1b5a307bb8d868'
+  };
+  next();
+}); 
 
-app.use(routes);
+mongoose.connect("mongodb://localhost:27017/aroundb");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(routes);
 
 app.listen(PORT, () => {
   console.log(`App initialised on port ${PORT}`);
